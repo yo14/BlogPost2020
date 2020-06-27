267be6f74f46a3f772ea1c294c0dd386dcf3dab0
yo14 <ifliandry11@gmail.com>
2020-06-27 21:32:18 +0700

    Create all routes + pages as starting point

--- app.py
+++ app.py
@@ -8,5 +8,41 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/blogs/<int:id>')
+def blogs(id):
+    return render_template('blogs.html', blog_id=id)
+
+@app.route('/register/')
+def register():
+    return render_template('register.html')
+
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/write-blog/', methods=['GET','POST'])
+def write_blog():
+    return render_template('write-blog.html')
+
+@app.route('/my-blogs/')
+def my_blogs():
+    return render_template('my-blogs.html')
+
+@app.route('/edit-blog/<int:id>/', methods=['GET','POST'])
+def edit_blog():
+    return render_template('edit-blog.html')
+
+@app.route('/delete-blog/<int:id>/', methods=['POST'])
+def delete_blog():
+    return 'Success'
+
+@app.route('/logout/')
+def logout():
+    return render_template('logout.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
